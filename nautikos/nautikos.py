81af0d5f440636e1649825b8f99f31ec7f146eb8
Jan Hein de Jong <janhein.dejong@gmail.com>
2023-03-21 09:55:29 +0100

    Add poethepoet

--- nautikos/nautikos.py
+++ nautikos/nautikos.py
@@ -1,23 +1,48 @@
+from typing import Any, TypedDict
+
 import typer
 import yaml
 
 app = typer.Typer()
 
 
+class Environment(TypedDict):
+    name: str
+    manifests: list[str]
+
+
+class Image(TypedDict):
+    repository: str
+    environments: list[Environment]
+
+
+class Config(TypedDict):
+    images: list[Image]
+
+
 @app.command()
 def main(
-    image: str, 
-    tag: str, 
-    env: str = typer.Option(None), 
+    image: str,
+    tag: str,
+    env: str = typer.Option(None),
     dry_run: bool = typer.Option(False, "--dry-run"),
-    config: str = typer.Option("nautikos.yaml")
-): 
-    config_data = _parse_yaml(config) 
+    config: str = typer.Option("nautikos.yaml"),
+):
+    config_data = _parse_yaml(config)
     print(config_data)
 
+    # For image in images:
+    # For environment
+    # For file in files
+    # Detect type
+    # Replace or print
 
 
-def _parse_yaml(filepath: str) -> dict:
-    with open(filepath, "r") as f: 
+def _read_config(config: str) -> Config:
+    return _parse_yaml(config)
+
+
+def _parse_yaml(filepath: str) -> Any:
+    with open(filepath, "r") as f:
         data = yaml.safe_load(f.read())
-    return data 
+    return data
