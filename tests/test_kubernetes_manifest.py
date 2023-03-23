c3bd51621deb5fa49e1138b03f2e876180090c2e
Jan Hein de Jong <janhein.dejong@gmail.com>
2023-03-23 09:43:50 +0100

    Forgot to git add tests

---
+++ tests/test_kubernetes_manifest.py
@@ -0,0 +1,114 @@
+import io
+
+import pytest
+
+from nautikos.nautikos import KubernetesManifest
+
+INPUT = """# Comment
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  labels:
+    app: kubernetes-example
+  name: kubernetes-example
+spec:
+  replicas: 3
+  selector:
+    matchLabels:
+      app: kubernetes-example
+  template:
+    metadata:
+      labels:
+        app: kubernetes-example
+    spec:
+      containers:
+      - image: some-repository:1.0.0 # Inline comment
+        name: service-kubernetes-example-1 
+        ports:
+        - containerPort: 8000
+      - image: some-repository
+        name: service-kubernetes-example-2
+      - image: some-other-repository:1.2.3
+        name: some-other-service
+"""
+
+OUTPUT = """# Comment
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  labels:
+    app: kubernetes-example
+  name: kubernetes-example
+spec:
+  replicas: 3
+  selector:
+    matchLabels:
+      app: kubernetes-example
+  template:
+    metadata:
+      labels:
+        app: kubernetes-example
+    spec:
+      containers:
+      - image: some-repository:1.1   # Inline comment
+        name: service-kubernetes-example-1
+        ports:
+        - containerPort: 8000
+      - image: some-repository:1.1
+        name: service-kubernetes-example-2
+      - image: some-other-repository:1.2.3
+        name: some-other-service
+"""
+
+REPOSITORY = "some-repository"
+TAG = "1.1"
+
+
+@pytest.fixture(scope="class")
+def input_file() -> io.StringIO:
+    f = io.StringIO(INPUT)
+    return f
+
+
+@pytest.fixture(scope="class")
+def output_file() -> io.StringIO:
+    f = io.StringIO()
+    return f
+
+
+@pytest.fixture(scope="class")
+def manifest(input_file: io.StringIO) -> KubernetesManifest:
+    kubernetes_handler = KubernetesManifest()
+    kubernetes_handler.load(input_file)
+    return kubernetes_handler
+
+
+class TestKubernetesManifest:
+    @pytest.fixture(autouse=True, scope="class")
+    def modify(self, manifest: KubernetesManifest):
+        manifest.modify(REPOSITORY, TAG)
+
+    @pytest.fixture(autouse=True, scope="class")
+    def write(self, manifest: KubernetesManifest, output_file):
+        manifest.write(output_file)
+
+    def test_tag_applied_correctly(self, manifest: KubernetesManifest):
+        assert manifest.get_images()[0] == {
+            "repository": REPOSITORY,
+            "tag": TAG,
+        }
+
+    def test_no_tag_handled_correctly(self, manifest: KubernetesManifest):
+        assert manifest.get_images()[1] == {
+            "repository": REPOSITORY,
+            "tag": TAG,
+        }
+
+    def test_different_image_untouched(self, manifest: KubernetesManifest):
+        assert manifest.get_images()[2] == {
+            "repository": "some-other-repository",
+            "tag": "1.2.3",
+        }
+
+    def test_write(self, output_file: io.StringIO):
+        assert output_file.getvalue() == OUTPUT
---
+++ tests/test_kustomize_manifest.py
@@ -0,0 +1,78 @@
+import io
+
+import pytest
+
+from nautikos.nautikos import KustomizeManifest
+
+INPUT = """# Comment
+resources:
+- ../some/path
+images:
+- name: some-repository
+  newTag: '1.0.0'
+- name: some-other-repository
+  newTag: '1.2.3'
+
+patchesStrategicMerge:
+- ingress.yml
+"""
+
+OUTPUT = """# Comment
+resources:
+- ../some/path
+images:
+- name: some-repository
+  newTag: '1.1'
+- name: some-other-repository
+  newTag: '1.2.3'
+
+patchesStrategicMerge:
+- ingress.yml
+"""
+
+REPOSITORY = "some-repository"
+TAG = "1.1"
+
+
+@pytest.fixture(scope="class")
+def input_file() -> io.StringIO:
+    f = io.StringIO(INPUT)
+    return f
+
+
+@pytest.fixture(scope="class")
+def output_file() -> io.StringIO:
+    f = io.StringIO()
+    return f
+
+
+@pytest.fixture(scope="class")
+def manifest(input_file: io.StringIO) -> KustomizeManifest:
+    manifest = KustomizeManifest()
+    manifest.load(input_file)
+    return manifest
+
+
+class TestKustomizeManifest:
+    @pytest.fixture(autouse=True, scope="class")
+    def modify(self, manifest: KustomizeManifest):
+        manifest.modify(REPOSITORY, TAG)
+
+    @pytest.fixture(autouse=True, scope="class")
+    def write(self, manifest: KustomizeManifest, output_file):
+        manifest.write(output_file)
+
+    def test_tag_applied_correctly(self, manifest: KustomizeManifest):
+        assert manifest.get_images()[0] == {
+            "repository": REPOSITORY,
+            "tag": TAG,
+        }
+
+    def test_different_image_untouched(self, manifest: KustomizeManifest):
+        assert manifest.get_images()[1] == {
+            "repository": "some-other-repository",
+            "tag": "1.2.3",
+        }    
+
+    def test_write(self, output_file: io.StringIO):
+        assert output_file.getvalue() == OUTPUT
